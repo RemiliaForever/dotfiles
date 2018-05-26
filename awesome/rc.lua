f4137981e6b2d7ef4056f769519532d575906732
RemiliaForever <remilia@koumakan.cc>
2018-05-26 14:24:00 +0800

    daily backup

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -73,8 +73,8 @@ modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-    awful.layout.suit.tile.left,
-    -- awful.layout.suit.tile,
+    awful.layout.suit.tile,
+    -- awful.layout.suit.tile.left,
     awful.layout.suit.tile.top,
     -- awful.layout.suit.tile.bottom,
     awful.layout.suit.fair,
@@ -717,6 +717,7 @@ awful.rules.rules = {
           "Steam",
           "Display",
           "Animation",
+          "matplotlib",
           },
         name = {
           "Event Tester",  -- xev.
