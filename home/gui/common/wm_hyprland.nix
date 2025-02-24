fb43300b2c6d5a78765095d0f7e01180199fe109
RemiliaForever <remilia@koumakan.cc>
2025-02-24 15:28:34 +0800

    update nixos-unstable

--- home/gui/common/wm_hyprland.nix
+++ home/gui/common/wm_hyprland.nix
@@ -204,8 +204,6 @@
         "float, class:(wechat), title:(预览)"
         "float, class:(nm-connection-editor)"
         "float, class:(.blueman-manager-wrapped)"
-
-        "renderunfocused, class:(starrail.exe)" # fix fps limit
       ];
 
       workspace = [ ];
--- overlays/patch/default.nix
+++ overlays/patch/default.nix
@@ -1,11 +1,4 @@
 final: prev:
 
 {
-  #380267
-  feishu = prev.feishu.overrideAttrs (old: {
-    installPhase = ''
-      ${old.installPhase}
-      rm $out/opt/bytedance/feishu/libcurl.so
-    '';
-  });
 }
