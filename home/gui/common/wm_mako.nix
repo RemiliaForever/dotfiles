11ae13f0433db2c01f97a1f62840870b29b560dc
RemiliaForever <remilia@koumakan.cc>
2025-06-15 14:42:41 +0800

    [home][gui] update mako style

--- home/gui/common/wm_mako.nix
+++ home/gui/common/wm_mako.nix
@@ -5,17 +5,22 @@
     enable = true;
 
     settings = {
-      actions = 1;
       default-timeout = 10000;
-      ignore-timeout = 0;
       layer = "overlay";
       sort = "-time";
 
-      icons = 1;
-      background-color = "#595959aa";
-      border-color = "#33ccffee";
+      background-color = "#212121dd";
       border-radius = 8;
       border-size = 2;
+
+      "urgency=low".border-color = "#595959ee";
+      "urgency=normal".border-color = "#33ccffee";
+      "urgency=high" = {
+        border-color = "#f38ba8ee";
+        default-timeout = 0;
+      };
+
+      on-notify = "exec mpv /run/current-system/sw/share/sounds/freedesktop/stereo/bell.oga";
     };
   };
 }
