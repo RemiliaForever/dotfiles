ffdfd8ef6f20de4dd6094e424fd7bc1dd52e1d82
RemiliaForever <remilia@koumakan.cc>
2025-08-08 10:35:48 +0800

    [home][cli] update copilot model to gpt5

--- home/cli/neovim/lua/plugin-config/_nvim-cmp.lua
+++ home/cli/neovim/lua/plugin-config/_nvim-cmp.lua
@@ -38,7 +38,7 @@ local kind_icons = {
 copilot.setup({
 	suggestion = { enabled = false },
 	panel = { enabled = false },
-	copilot_mode = "gpt-4.1",
+	copilot_mode = "gpt-5",
 })
 copilot_cmp.setup({})
 
