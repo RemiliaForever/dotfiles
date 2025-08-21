c3d9a80aed24a5858f310c1cfde7ab571f573c92
RemiliaForever <remilia@koumakan.cc>
2025-08-21 10:28:01 +0800

    [hosts][wsl] update rsync alias

--- hosts/wsl-arm64/home/default.nix
+++ hosts/wsl-arm64/home/default.nix
@@ -12,9 +12,8 @@
   # cli
   programs.starship.settings.format = lib.mkForce "wsl $directory $character";
   programs.zsh.shellAliases = {
-    s = ''
-      rsync -aP --delete "$PWD/" "/mnt/c/Users/''${PWD#/home/}/" --exclude ".git"
-    '';
+    s = ''rsync -aP --delete "$PWD/" "/mnt/c/Users/''${PWD#/home/}/" --exclude ".git" --exclude ".direnv" --exclude "build"'';
+    sb = ''rsync -aP --delete "$PWD/" "/mnt/c/Users/''${PWD#/home/}/" --exclude ".git" --exclude ".direnv"'';
   };
 
   home.sessionVariables = {
