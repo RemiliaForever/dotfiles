0fd593cb554a6d1e9fe32ffac7fa0431d9dbfd57
RemiliaForever <remilia@koumakan.cc>
2026-01-10 16:51:55 +0800

    [overlay][path] update qq

--- overlays/patch/pr.nix
+++ overlays/patch/pr.nix
@@ -12,4 +12,13 @@ final: prev:
         ./niri/0002-Unify-force-render-logic-and-ensure-each-window-uses.patch
       ];
   });
+
+  # https://github.com/NixOS/nixpkgs/pull/478345
+  qq = prev.qq.overrideAttrs (oldAttrs: {
+    version = "3.2.23-2026-01-08";
+    src = final.fetchurl {
+      url = "https://dldir1v6.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.23_260108_amd64_01.deb";
+      hash = "sha256-pCUnGcG+uK3ODaCev8MQzlDHnqVI9czkKVBXZdC/uoQ=";
+    };
+  });
 }
