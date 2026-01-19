ba949825604ed0ebc37b3ee5d6b39f636170e09c
RemiliaForever <remilia@koumakan.cc>
2026-01-20 00:22:50 +0800

    [overlay][patch] add niri shm screenshare patch

--- overlays/patch/pr.nix
+++ overlays/patch/pr.nix
@@ -10,6 +10,10 @@ final: prev:
       [
         ./niri/0001-add-feat-force-render.patch
         ./niri/0002-Unify-force-render-logic-and-ensure-each-window-uses.patch
+        (final.fetchpatch {
+          url = "https://github.com/wrvsrx/niri/compare/tag_support-shm-sharing_2~19..tag_support-shm-sharing_2.patch";
+          hash = "sha256-M2Z2HMwuJpDtk7bvvREXF21cHVra+qqUUeaKCywLt48=";
+        })
       ];
   });
 
