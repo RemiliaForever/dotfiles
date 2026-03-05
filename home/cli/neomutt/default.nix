c5263b1a0b058cd4b7e42b3c8bf2d6deb38f226f
RemiliaForever <remilia@koumakan.cc>
2026-03-05 13:34:32 +0800

    [home][cli] add mutt alias

--- home/cli/neomutt/default.nix
+++ home/cli/neomutt/default.nix
@@ -11,4 +11,9 @@
   home.activation.neomutt = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     ${pkgs.coreutils}/bin/mkdir -p $HOME/.cache/neomutt/messages
   '';
+
+  programs.zsh.shellAliases = {
+    mutt-koumakan = "neomutt -e 'source ~/.config/neomutt/koumakan'";
+    mutt-nexa = "neomutt -e 'source ~/.config/neomutt/nexa4ai'";
+  };
 }
