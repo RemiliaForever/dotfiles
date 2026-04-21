e219203896b9d1cbbdf6e15ff43e46eec1c7f53c
RemiliaForever <remilia@koumakan.cc>
2026-04-21 15:58:52 +0800

    [home][cli] update claude-code to claude-code-bin

--- home/cli/tool.nix
+++ home/cli/tool.nix
@@ -9,7 +9,7 @@
     rsync
 
     github-copilot-cli
-    claude-code
+    claude-code-bin
   ];
 
   # comma
@@ -58,7 +58,7 @@
     c = "copilot --model gpt-5-mini --reasoning-effort low";
     cc = "copilot --model gpt-5.4 --autopilot";
     a = "claude --model us.anthropic.claude-sonnet-4-6 --effort low --allow-dangerously-skip-permissions";
-    aa = "claude --model 'us.anthropic.claude-opus-4-6-v1[1m]' --effort high --permission-mode bypassPermissions";
+    aa = "claude --model 'us.anthropic.claude-opus-4-7[1m]' --effort high --permission-mode bypassPermissions";
   };
 
   programs.fzf = {
--- overlays/patch/update.nix
+++ overlays/patch/update.nix
@@ -11,4 +11,19 @@ final: prev:
       hash = "sha256-TOTo3Oyf2vWqOz/Jn1+o7I0ym8DM6+lzy/jDq1uAnFw=";
     };
   });
+
+  claude-code-bin = prev.claude-code-bin.overrideAttrs (
+    oldAttrs:
+    let
+      baseUrl = "https://storage.googleapis.com/claude-code-dist-86c565f3-f756-42ad-8dfa-d59b1c096819/claude-code-releases";
+      platformKey = "${final.stdenv.hostPlatform.node.platform}-${final.stdenv.hostPlatform.node.arch}";
+    in
+    rec {
+      version = "2.1.116";
+      src = final.fetchurl {
+        url = "${baseUrl}/${version}/${platformKey}/claude";
+        sha256 = "sha256-DRrqXOBWpc5JHafpu+Y/mSWF5cJIUvAjoHyPGM8pLMU=";
+      };
+    }
+  );
 }
