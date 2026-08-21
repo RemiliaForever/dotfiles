2e08f8e57860525c1b5b3a941e216a05c3479ec9
RemiliaForever <remilia@koumakan.cc>
2026-08-21 19:01:48 +0800

    [overlays][patch] update claude-code to 2.1.238

--- overlays/patch/update.nix
+++ overlays/patch/update.nix
@@ -11,4 +11,23 @@ final: prev:
       hash = "sha256-J6oUMUzfogsIBj1GpwWmKhjphTV628rG+3w28Dc81Fw=";
     };
   });
+
+  claude-code = prev.claude-code.overrideAttrs (oldAttrs: rec {
+    version = "2.1.238";
+    src =
+      let
+        inherit (final.stdenv.hostPlatform.node) platform arch;
+      in
+      final.fetchurl {
+        url = "https://storage.googleapis.com/claude-code-dist-86c565f3-f756-42ad-8dfa-d59b1c096819/claude-code-releases/${version}/${platform}-${arch}/claude";
+        sha256 =
+          {
+            "darwin-arm64" = "1c196c456373b57818ae87df84aecee96cb659448c0d6a6bbb401ac5758431b2";
+            "darwin-x64" = "d10bc7bb1720435f8830aa3ee74085f09348d2b1a2a152bdee251b770d76cc73";
+            "linux-arm64" = "28d736120a6b14c5eae1ad1470e73371818c9c2fa41e0b3c7040207aa2d4edee";
+            "linux-x64" = "0933b286cf94e1b2504b35ac165ab76b8f822735d53371c56393988c23040d58";
+          }
+          ."${platform}-${arch}";
+      };
+  });
 }
