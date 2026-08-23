123deda98a28d15f686a38d7b3ab65353a2a0176
RemiliaForever <remilia@koumakan.cc>
2026-08-23 14:32:15 +0800

    [overlays][patch] patch waybar to accept a fractional interval
    https://github.com/Alexays/Waybar/pull/4895 -- interval was cast to int, so
    any value below 1 became 0.

--- overlays/patch/pr.nix
+++ overlays/patch/pr.nix
@@ -3,4 +3,14 @@ final: prev:
 {
   # https://github.com/YaLTeR/niri/pull/2609
   # https://github.com/YaLTeR/niri/pull/1791
+
+  # https://github.com/Alexays/Waybar/pull/4895
+  waybar = prev.waybar.overrideAttrs (oldAttrs: {
+    patches = (oldAttrs.patches or [ ]) ++ [
+      (final.fetchpatch {
+        url = "https://github.com/Alexays/Waybar/commit/d929f1a62c764a4cff0897540bc00bf24beb0d5e.patch";
+        hash = "sha256-xQxUdw7kXBeGkwFt9Brz7hVOcl2KiimEMPym+0/HCVg=";
+      })
+    ];
+  });
 }
