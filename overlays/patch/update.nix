ada4107d0c41218e9ee7f39f085c443869a424c6
RemiliaForever <remilia@koumakan.cc>
2026-08-30 18:12:59 +0800

    [overlays][patch] update claude-code to 2.1.251

--- overlays/patch/update.nix
+++ overlays/patch/update.nix
@@ -13,7 +13,7 @@ final: prev:
   });
 
   claude-code = prev.claude-code.overrideAttrs (oldAttrs: rec {
-    version = "2.1.238";
+    version = "2.1.251";
     src =
       let
         inherit (final.stdenv.hostPlatform.node) platform arch;
@@ -22,10 +22,10 @@ final: prev:
         url = "https://storage.googleapis.com/claude-code-dist-86c565f3-f756-42ad-8dfa-d59b1c096819/claude-code-releases/${version}/${platform}-${arch}/claude";
         sha256 =
           {
-            "darwin-arm64" = "1c196c456373b57818ae87df84aecee96cb659448c0d6a6bbb401ac5758431b2";
-            "darwin-x64" = "d10bc7bb1720435f8830aa3ee74085f09348d2b1a2a152bdee251b770d76cc73";
-            "linux-arm64" = "28d736120a6b14c5eae1ad1470e73371818c9c2fa41e0b3c7040207aa2d4edee";
-            "linux-x64" = "0933b286cf94e1b2504b35ac165ab76b8f822735d53371c56393988c23040d58";
+            "darwin-arm64" = "625869b01e0050f260b2980fac248fd9cef9e462612bded4ec9d3d49ff8969a5";
+            "darwin-x64" = "44221d72a3f35772faa85ad9a36a678084a516f720e64b45e26eb9015315500b";
+            "linux-arm64" = "65445bd4dd042079cc3fa43791b561370a05c8599e8ec47580e25a81050abbdd";
+            "linux-x64" = "fd5f10ff0eb58daec04900466b143ea98aab50abf208a422bc008eaec13f61f7";
           }
           ."${platform}-${arch}";
       };
