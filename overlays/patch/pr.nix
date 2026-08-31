63d8949899329168de0df70f7335632e9ebd928e
RemiliaForever <remilia@koumakan.cc>
2026-08-31 18:36:06 +0800

    [overlays][patch] stop waybar mpris from starving gtk tooltips

--- overlays/patch/pr.nix
+++ overlays/patch/pr.nix
@@ -11,6 +11,9 @@ final: prev:
         url = "https://github.com/Alexays/Waybar/commit/d929f1a62c764a4cff0897540bc00bf24beb0d5e.patch";
         hash = "sha256-xQxUdw7kXBeGkwFt9Brz7hVOcl2KiimEMPym+0/HCVg=";
       })
+      # not upstream yet: mpris reset the display-wide GTK tooltip timer on
+      # every update, so a sub-500ms interval killed tooltips for the whole bar
+      ./waybar/mpris-tooltip.patch
     ];
   });
 }
